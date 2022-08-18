c2584d9e8435d8c5d418a6a0634011a990ad6666
Julian Garcete <julianyyiyo@outlook.com>
2022-08-18 08:52:40 -0300

    😎

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,10 +1,54 @@
-export const register = (req, res) => {
+import { User } from "../models/user.js";
+import jwt from 'jsonwebtoken'
 
-    console.log(req.body)
-    res.json({ok: "Register"})
-}
 
-export const login = (req, res) => {
-    res.json({ ok: "Login"});
+export const register = async(req, res) => {
+    const {email, password} = req.body;
+    try {
+       
+        const user = new User({email, password});
+
+
+        await user.save();
+        return res.status(201).json({ok: true});
+
+
+    } catch (error) {
+        console.log(error.code);
+        if(error.code === 11000){
+            return res.status(400).json({ error: "Ya existe este Usuario" });
+        }
+
+        return res.status(500).json({ error: "Error de Servidor" })
+
+    }
+    
+};
+
+
+export const login =  async (req, res) => {
+try{
+    const {email, password} = req.body;
+
+    let user = await User.findOne({email});
+
+    if(!user)  return res.status(403).json({ error: "No Existe este Usuario"});
+
+
+    const respuestaPassword = await user.comparePassword(password)
+    if (!respuestaPassword)
+    return res.status(403).json({ error: "Contraseña Incorrecta"})
+
+    //Token de JWT
+    const token = jwt.sign({uid: user.id}, process.env.JWT_SECRET);
+
+
+   return res.json({ token});
 }
+catch (error) {
+    console.log(error)
+
+    return res.status(500).json({ error: "Error de Servidor" })
+}
+};
 
--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcryptjs from "bcryptjs";
 const {Schema, model} = mongoose;
 
 const userSchema = new Schema({
@@ -14,6 +15,28 @@ const userSchema = new Schema({
         type: String,
         required: true,
     }
-})
+});
 
-export const User = model('user', userSchema)
+userSchema.pre("save", async function(next){
+    const user  = this
+
+
+    if(!user.isModified('password')) return next()
+    try {
+
+        const salt = await bcryptjs.genSalt(10)
+        user.password = await bcryptjs.hash(user.password, salt)
+        next()
+    }
+    catch (error) {
+          console.log(error)
+          throw new Error('Fallo el hash de Contraseña')
+    }
+
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcryptjs.compare(candidatePassword, this.password);
+};
+
+export const User = model('User', userSchema)
